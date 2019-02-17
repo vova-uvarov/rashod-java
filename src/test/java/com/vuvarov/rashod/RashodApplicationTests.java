be4385f8de454604a06ef1f02369528c30db0986
vova.uvarov <vova.uvarov@bssys.com>
2019-02-17 18:45:04 +0300

    init

---
+++ src/test/java/com/vuvarov/rashod/RashodApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vuvarov.rashod;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RashodApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
