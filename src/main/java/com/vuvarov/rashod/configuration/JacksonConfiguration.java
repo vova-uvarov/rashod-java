8c185c01fe8422ee91ada7a09ffdc1f031d5b3bf
vova.uvarov <vova.uvarov@bssys.com>
2019-09-03 23:48:42 +0300

    fix date serialize

--- src/main/java/com/vuvarov/rashod/configuration/JacksonConfiguration.java
+++ src/main/java/com/vuvarov/rashod/configuration/JacksonConfiguration.java
@@ -1,6 +1,8 @@
 package com.vuvarov.rashod.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,15 +12,19 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 public class JacksonConfiguration {
     @Bean
     public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-        return new Jackson2ObjectMapperBuilder() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder() {
 
             @Override
             public void configure(ObjectMapper objectMapper) {
                 super.configure(objectMapper);
+                objectMapper.registerModule(new Jdk8Module());
                 objectMapper.registerModule(new JavaTimeModule());
             }
 
         };
+        builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
+        return builder;
     }
+
 }
