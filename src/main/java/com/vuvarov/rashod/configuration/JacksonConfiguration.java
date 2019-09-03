5f264344eda5b6d1623b23c82338c9d54f9900f4
uvarovvy <uvarov.vy@reliab.tech>
2019-09-03 17:38:01 +0300

    register javaTimeModule

---
+++ src/main/java/com/vuvarov/rashod/configuration/JacksonConfiguration.java
@@ -0,0 +1,24 @@
+package com.vuvarov.rashod.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class JacksonConfiguration {
+    @Bean
+    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder() {
+
+            @Override
+            public void configure(ObjectMapper objectMapper) {
+                super.configure(objectMapper);
+                objectMapper.registerModule(new JavaTimeModule());
+            }
+
+        };
+
+    }
+}
