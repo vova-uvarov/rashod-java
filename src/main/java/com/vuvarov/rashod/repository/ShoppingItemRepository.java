8d08fa857ab8de037b51ef4ee2caca6e2c279cbf
uvarovvy <uvarov.vy@reliab.tech>
2019-09-03 16:54:02 +0300

    api для поиска ShoppingItem

--- src/main/java/com/vuvarov/rashod/repository/ShoppingItemRepository.java
+++ src/main/java/com/vuvarov/rashod/repository/ShoppingItemRepository.java
@@ -1,9 +1,15 @@
 package com.vuvarov.rashod.repository;
 
 import com.vuvarov.rashod.model.ShoppingItem;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ShoppingItemRepository extends PagingAndSortingRepository<ShoppingItem, Long> {
+    @Query("SELECT c FROM ShoppingItem c WHERE LOWER(c.name) LIKE LOWER(?1)")
+    List<ShoppingItem> findAllByName(String q, Pageable pageable);
 }
