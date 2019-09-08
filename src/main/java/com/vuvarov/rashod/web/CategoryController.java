2aeb65e00e5431ca31093c2375f93d432f023d14
vova.uvarov <vova.uvarov@bssys.com>
2019-09-08 11:08:18 +0300

    sort

--- src/main/java/com/vuvarov/rashod/web/CategoryController.java
+++ src/main/java/com/vuvarov/rashod/web/CategoryController.java
@@ -3,6 +3,7 @@ package com.vuvarov.rashod.web;
 import com.vuvarov.rashod.model.Category;
 import com.vuvarov.rashod.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,5 +19,10 @@ public class CategoryController extends RestRepositoryController<Category, Long,
     @GetMapping("/search")
     public Iterable<Category> search(@RequestParam String q, @PageableDefault Pageable pageable) {
         return repository.findAllByName("%" + q + "%", pageable);
+    }
+
+    @Override
+    public Page<Category> findAll(@PageableDefault(sort = "name") Pageable pageable) {
+        return super.findAll(pageable);
     }
 }
