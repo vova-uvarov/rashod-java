2359cd2f7a8cb9ce4cdc61d3d12c192936a98961
vova.uvarov <vova.uvarov@bssys.com>
2019-09-08 09:42:29 +0300

    little fix operation filters

--- src/main/java/com/vuvarov/rashod/web/dto/OperationFilterDto.java
+++ src/main/java/com/vuvarov/rashod/web/dto/OperationFilterDto.java
@@ -1,12 +1,9 @@
 package com.vuvarov.rashod.web.dto;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.vuvarov.rashod.model.ShoppingItem;
 import com.vuvarov.rashod.model.enums.AccountType;
 import com.vuvarov.rashod.model.enums.Currency;
 import com.vuvarov.rashod.model.enums.OperationType;
-import com.vuvarov.rashod.util.LocalDate2LocalDateTimeDeserializer;
-import com.vuvarov.rashod.util.LocalDateTime2DateSerializer;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
@@ -14,7 +11,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -30,10 +26,12 @@ public class OperationFilterDto {
     BigDecimal costTo;
     List<OperationType> operationTypes;
     List<AccountType> accountTypes;
+    List<String> shoppingList;
     List<Long> categoryIds;
-    Long accountId;
+    List<Long> accountIds;
     String tag;
     Boolean isPlan;
     String place;
+    String comment;
     Currency currency;
 }
