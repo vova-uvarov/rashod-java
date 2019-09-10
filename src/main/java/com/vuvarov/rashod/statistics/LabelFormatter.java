323f4c77d4a22470b921954a0fa862b2ef9dc841
uvarovvy <uvarov.vy@reliab.tech>
2019-09-10 12:52:10 +0300

    add groupBy to statistics

---
+++ src/main/java/com/vuvarov/rashod/statistics/LabelFormatter.java
@@ -0,0 +1,26 @@
+package com.vuvarov.rashod.statistics;
+
+import com.vuvarov.rashod.web.dto.statistics.StatisticsGroupBy;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class LabelFormatter {
+    private static final DateTimeFormatter DF_MONTH = DateTimeFormatter.ofPattern("yyyy-MMM");
+    private static final DateTimeFormatter DF_YEAR = DateTimeFormatter.ofPattern("yyyy");
+    private static final DateTimeFormatter DF_DAY = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public String format(LocalDateTime date, StatisticsGroupBy groupBy) {
+        if (StatisticsGroupBy.YEAR.equals(groupBy)) {
+            return DF_YEAR.format(date);
+        }
+
+        if (StatisticsGroupBy.DAY.equals(groupBy)) {
+            return DF_DAY.format(date);
+        }
+
+        return DF_MONTH.format(date);
+    }
+}
