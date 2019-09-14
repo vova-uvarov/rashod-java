f045c1c4c88109e7c0dde6e97f1f6a1c389a4423
vova.uvarov <vova.uvarov@bssys.com>
2019-09-14 15:27:31 +0300

    extend dictionary api

--- src/main/java/com/vuvarov/rashod/web/DictionaryController.java
+++ src/main/java/com/vuvarov/rashod/web/DictionaryController.java
@@ -1,5 +1,8 @@
 package com.vuvarov.rashod.web;
 
+import com.vuvarov.rashod.model.enums.AccountStatus;
+import com.vuvarov.rashod.model.enums.AccountType;
+import com.vuvarov.rashod.model.enums.Currency;
 import com.vuvarov.rashod.model.enums.OperationType;
 import com.vuvarov.rashod.service.interfaces.IAccountService;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +14,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/dictionary")
 @RequiredArgsConstructor
-public class DictionaryController  {
+public class DictionaryController {
 
     private final IAccountService accountService;
 
     @GetMapping("/operationTypes")
-    List<OperationType> operationTypes(){
+    List<OperationType> operationTypes() {
         return Arrays.asList(OperationType.values());
+    }
+
+    @GetMapping("/accountTypes")
+    List<AccountType> accountTypes() {
+        return Arrays.asList(AccountType.values());
+    }
+
+    @GetMapping("/currencies")
+    List<Currency> currencies() {
+        return Arrays.asList(Currency.values());
+    }
+
+    @GetMapping("/accountStatuses")
+    List<AccountStatus> accountStatuses() {
+        return Arrays.asList(AccountStatus.values());
     }
 }
