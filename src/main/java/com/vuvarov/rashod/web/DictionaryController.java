78f09f23a21e1a0e4df6a7f2b02b91604d49c7a2
vova.uvarov <vova.uvarov@bssys.com>
2019-09-16 23:09:55 +0300

    years dictionary

--- src/main/java/com/vuvarov/rashod/web/DictionaryController.java
+++ src/main/java/com/vuvarov/rashod/web/DictionaryController.java
@@ -5,18 +5,23 @@ import com.vuvarov.rashod.model.enums.AccountType;
 import com.vuvarov.rashod.model.enums.Currency;
 import com.vuvarov.rashod.model.enums.OperationType;
 import com.vuvarov.rashod.service.interfaces.IAccountService;
+import com.vuvarov.rashod.service.interfaces.IOperationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("/api/dictionary")
 @RequiredArgsConstructor
 public class DictionaryController {
 
-    private final IAccountService accountService;
+    private final IOperationService operationService;
 
     @GetMapping("/operationTypes")
     List<OperationType> operationTypes() {
@@ -36,5 +41,13 @@ public class DictionaryController {
     @GetMapping("/accountStatuses")
     List<AccountStatus> accountStatuses() {
         return Arrays.asList(AccountStatus.values());
+    }
+
+    @GetMapping("/years")
+    List<Integer> years() {
+        return IntStream.rangeClosed(operationService.minOperationDate().getYear(), LocalDate.now().getYear())
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
     }
 }
