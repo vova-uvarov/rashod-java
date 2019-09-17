655da156d0cbd7f8bcabac37f5d45cf1c57803d1
vova.uvarov <vova.uvarov@bssys.com>
2019-09-17 23:08:31 +0300

    add paramGroups and paramKeys to static dictionaries

--- src/main/java/com/vuvarov/rashod/model/param/ParamKey.java
+++ src/main/java/com/vuvarov/rashod/model/param/ParamKey.java
@@ -1,14 +1,5 @@
 package com.vuvarov.rashod.model.param;
 
-import lombok.Getter;
-
 public enum ParamKey {
-    SUM_TO_MONTH("План на месяц");
-
-    @Getter
-    private final String name;
-
-    ParamKey(String s) {
-        this.name = s;
-    }
+    SUM_TO_MONTH;
 }
