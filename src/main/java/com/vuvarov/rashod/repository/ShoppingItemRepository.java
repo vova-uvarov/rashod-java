a2b2032833cd17c064799abd69211c1aab188a78
vova.uvarov <vova.uvarov@bssys.com>
2019-09-21 15:56:12 +0300

    shoppig item names divide by operation type

--- src/main/java/com/vuvarov/rashod/repository/ShoppingItemRepository.java
+++ src/main/java/com/vuvarov/rashod/repository/ShoppingItemRepository.java
@@ -1,9 +1,11 @@
 package com.vuvarov.rashod.repository;
 
 import com.vuvarov.rashod.model.ShoppingItem;
+import com.vuvarov.rashod.model.enums.OperationType;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,6 +17,9 @@ public interface ShoppingItemRepository extends PagingAndSortingRepository<Shopp
 
     List<ShoppingItem> findAllByOperationId(Long operationId);
 
-    @Query("SELECT distinct c.name FROM ShoppingItem c order by c.name")
-    List<String> findAllNames();
+    @Query("SELECT distinct c.name FROM ShoppingItem c" +
+            " join Operation o on o.id=c.operationId" +
+            " where o.operationType=:operationType" +
+            " order by c.name")
+    List<String> findAllNamesByOperationType(@Param("operationType")OperationType operationType);
 }
