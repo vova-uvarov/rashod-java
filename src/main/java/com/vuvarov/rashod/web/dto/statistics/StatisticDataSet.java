9d091d924db6895cd3308f601e9a4c88a3b168e4
vova.uvarov <vova.uvarov@bssys.com>
2019-12-26 01:55:51 +0300

    draw horizontal line in average graph

--- src/main/java/com/vuvarov/rashod/web/dto/statistics/StatisticDataSet.java
+++ src/main/java/com/vuvarov/rashod/web/dto/statistics/StatisticDataSet.java
@@ -1,19 +1,25 @@
 package com.vuvarov.rashod.web.dto.statistics;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class StatisticDataSet {
-    private String code;
-    private String name;
-    private List<BigDecimal> data; // todo для pie просто используем одно значение. Подумать над этим
+    String code;
+    String name;
+    List<BigDecimal> data; // todo для pie просто используем одно значение. Подумать над этим
+    List<LocalDate> dates;
 }
+
